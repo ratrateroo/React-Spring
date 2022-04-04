0e1f5a1dc67177f907194d22fc9ba2d8729e5c74
Mark <ratrateroo@gmail.com>
2022-04-04 23:01:44 +0800

    added flight services project

---
+++ flightservices/src/main/java/com/flightservices/entities/Flight.java
@@ -0,0 +1,56 @@
+package com.flightservices.entities;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Flight {
+	private int id;
+	private String flightNumber;
+	private String operatingAirlines;
+	private String departureCity;
+	private String arrivalCity;
+	private Date dateOfDeparture;
+	private Timestamp estimatedDepartureTime;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFlightNumber() {
+		return flightNumber;
+	}
+	public void setFlightNumber(String flightNumber) {
+		this.flightNumber = flightNumber;
+	}
+	public String getOperatingAirlines() {
+		return operatingAirlines;
+	}
+	public void setOperatingAirlines(String operatingAirlines) {
+		this.operatingAirlines = operatingAirlines;
+	}
+	public String getDepartureCity() {
+		return departureCity;
+	}
+	public void setDepartureCity(String departureCity) {
+		this.departureCity = departureCity;
+	}
+	public String getArrivalCity() {
+		return arrivalCity;
+	}
+	public void setArrivalCity(String arrivalCity) {
+		this.arrivalCity = arrivalCity;
+	}
+	public Date getDateOfDeparture() {
+		return dateOfDeparture;
+	}
+	public void setDateOfDeparture(Date dateOfDeparture) {
+		this.dateOfDeparture = dateOfDeparture;
+	}
+	public Timestamp getEstimatedDepartureTime() {
+		return estimatedDepartureTime;
+	}
+	public void setEstimatedDepartureTime(Timestamp estimatedDepartureTime) {
+		this.estimatedDepartureTime = estimatedDepartureTime;
+	}
+}
