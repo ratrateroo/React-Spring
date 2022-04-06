d6c6ced9d41862bd8fc0b5394a9d8d5fd8c63b47
Mark <ratrateroo@gmail.com>
2022-04-07 00:04:37 +0800

    updated rest controller

--- flightservices/src/main/java/com/flightservices/integration/ReservationRestController.java
+++ flightservices/src/main/java/com/flightservices/integration/ReservationRestController.java
@@ -1,12 +1,17 @@
 package com.flightservices.integration;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flightservices.dto.CreateReservationRequest;
@@ -19,6 +24,7 @@ import com.flightservices.repositories.PassengerRepository;
 import com.flightservices.repositories.ReservationRepository;
 
 @RestController
+@CrossOrigin
 public class ReservationRestController {
 	@Autowired
 	FlightRepository flightRepository;
@@ -29,15 +35,21 @@ public class ReservationRestController {
 	@Autowired
 	ReservationRepository reservationRepository;
 
-	@RequestMapping(value = "/flights", method = RequestMethod.GET)
-	public List<Flight> findFlights() {
-		return flightRepository.findAll();
-
+	@RequestMapping("/flights")
+	public List<Flight> findFlights(@RequestParam("from") String from,@RequestParam("to") String to,
+			@RequestParam("departureDate") @DateTimeFormat(pattern="MM-dd-yyyy") Date departureDate) {
+		return flightRepository.findFlights(from, to, departureDate);
 	}
+	
+	@RequestMapping("/flights/{id}")
+	public Flight findFlight(@PathVariable("id") int id) {
+		return flightRepository.findById(id).get();
+	}
+	
 
 	@RequestMapping(value = "/reservations", method = RequestMethod.POST)
 	@Transactional
-	public Reservation saveReservation(CreateReservationRequest request) {
+	public Reservation saveReservation(@RequestBody CreateReservationRequest request) {
 		Flight flight = flightRepository.findById(request.getFlightId()).get();
 
 		Passenger passenger = new Passenger();
@@ -65,7 +77,7 @@ public class ReservationRestController {
 	}
 
 	@RequestMapping(value = "/reservations", method = RequestMethod.PUT)
-	public Reservation updateReservation(UpdateReservationRequest request) {
+	public Reservation updateReservation(@RequestBody UpdateReservationRequest request) {
 
 		Reservation reservation = reservationRepository.findById(request.getId()).get();
 		reservation.setNumberOfBags(request.getNumberOfBags());
